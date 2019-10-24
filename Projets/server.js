413368926639938b3abae671f455fa8988847da1
PeronMaxime <38103096+PeronMaxime@users.noreply.github.com>
2019-10-24 20:00:22 +0200

    Delete server.js

--- Projets/server.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-var port = process.env['PORT'] || 8080;
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    res.end('<!DOCTYPE html><html><meta charset="utf-8"><title>Maxime Péron' +
-            "</title><b>Bienvenue chez moi !</b><br /><br />Site en construction");
-}).listen(port);
-console.log("Server ready to accept requests on port %d", port);
